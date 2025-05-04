a20488f15964dffaaf3cd8c6436fb3c44f3b7379
Michal Louda <mloud@seznam.cz>
2025-05-04 17:51:17 +0200

    random with probablities

--- Runtime/Core/Scripts/Extensions/EnumerableExtensions.cs
+++ Runtime/Core/Scripts/Extensions/EnumerableExtensions.cs
@@ -27,6 +27,28 @@ namespace OneDay.Core.Extensions
             return source.ElementAt(random.Next(count));
         }
         
+        public static T GetRandomWithProbabilities<T>(this IEnumerable<T> source, List<float> probabilities)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            var random = new Random();
+            var sourceList = source.ToList();
+            if (sourceList.Count != probabilities.Count)
+                throw new ArgumentException("Count of probabilities must match count of items in the list");
+
+            float total = probabilities.Sum();
+            float rnd = (float)(random.NextDouble() * total);
+
+            float cumulative = 0f;
+            for (int i = 0; i < probabilities.Count; i++)
+            {
+                cumulative += probabilities[i];
+                if (rnd < cumulative)
+                    return sourceList[i];
+            }
+
+            throw new InvalidOperationException("Random selection failed due to probability mismatch.");
+        }
+        
         public static List<T> GetRandomized<T>(this IEnumerable<T> source)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
