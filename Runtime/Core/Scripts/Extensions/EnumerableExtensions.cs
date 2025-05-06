33b8671c13dc5170567f2f0032328d897a185734
Michal Louda <mloud@seznam.cz>
2025-05-06 10:35:42 +0200

    enumerable extensions

--- Runtime/Core/Scripts/Extensions/EnumerableExtensions.cs
+++ Runtime/Core/Scripts/Extensions/EnumerableExtensions.cs
@@ -7,6 +7,7 @@ namespace OneDay.Core.Extensions
 {
     public static class EnumerableExtensions
     {
+        private static readonly Random _random = new Random();
         public static T GetRandomExcluding<T>(this IEnumerable<T> source, T except)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
@@ -22,31 +23,40 @@ namespace OneDay.Core.Extensions
         public static T GetRandom<T>(this IEnumerable<T> source)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
-            var random = new Random();
             int count = source.Count();
-            return source.ElementAt(random.Next(count));
+            return source.ElementAt(_random.Next(count));
         }
-        
-        public static T GetRandomWithProbabilities<T>(this IEnumerable<T> source, List<float> probabilities)
+
+        public static int GetRandomIndexWithProbabilities<T>(this IList<T> source, List<float> probabilities)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
-            var random = new Random();
-            var sourceList = source.ToList();
-            if (sourceList.Count != probabilities.Count)
+
+            if (source.Count != probabilities.Count)
                 throw new ArgumentException("Count of probabilities must match count of items in the list");
 
             float total = probabilities.Sum();
-            float rnd = (float)(random.NextDouble() * total);
+            if (total <= 0f)
+                throw new ArgumentException("Sum of probabilities must be greater than zero");
+            
+            
+            float rnd = (float)(_random.NextDouble() * total);
 
             float cumulative = 0f;
             for (int i = 0; i < probabilities.Count; i++)
             {
                 cumulative += probabilities[i];
                 if (rnd < cumulative)
-                    return sourceList[i];
+                    return i;
             }
 
             throw new InvalidOperationException("Random selection failed due to probability mismatch.");
+        }
+        
+        public static T GetRandomWithProbabilities<T>(this IEnumerable<T> source, List<float> probabilities)
+        {
+            var sourceToList = source.ToList();
+            var rndIndex = sourceToList.GetRandomIndexWithProbabilities(probabilities);
+            return sourceToList[rndIndex];
         }
         
         public static List<T> GetRandomized<T>(this IEnumerable<T> source)
